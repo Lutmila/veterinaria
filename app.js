536a0aa0fb08bb56457a5da2756a35dfe3b1a27f
Lutmila <celestegaray55@gmai.com>
2025-09-27 21:45:08 -0300

    permito cualquier origen

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const mascotaRoutes = require('./routes/mascota');
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: "*"
 }));
+
+
 app.options('*', cors());
 
 app.use(express.json());
