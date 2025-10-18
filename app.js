569a6e07eccca2b7e09e870fa4ea71dde8a0ad26
Lutmila <lutmilagaray57@gmail.com>
2025-10-17 22:17:54 -0300

    corrijo error para vercel

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const dbconect = require('./config/db');
 
 const clienteRoutes = require('./routes/cliente');
 const mascotaRoutes = require('./routes/mascota');
 
+const app = express();
+
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: "*"
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-
-app.options('*', cors());
 
 app.use(express.json());
 
-app.use('/clientes',clienteRoutes);
-app.use('/mascotas',mascotaRoutes);
+app.use('/clientes', clienteRoutes);
+app.use('/mascotas', mascotaRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("Servidor funcionando correctamente");
+app.get('/', (req, res) => {
+  res.send("Servidor funcionando correctamente");
 });
 
-dbconect().then(()=>{
-    app.listen(3000,()=>{
-        console.log('servidor corriendo en el puerto 3000')
-    })
-
-}).catch(err=>{
-    console.log('no se pudo conectar debido a un error en la base');
-    process.exit(1);
-
+dbconect().catch(err => {
+  console.error('No se pudo conectar a la base de datos:', err);
 });
 
 module.exports = app;
